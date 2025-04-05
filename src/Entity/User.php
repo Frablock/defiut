d721bf3b190bd143ec3f1933d3bdc085845338e2
Frablock <53878484+Frablock@users.noreply.github.com>
2025-04-05 21:33:00 +0200

    Implementing UserInterface in user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,17 @@ namespace App\Entity;
 use Doctrine\Collections\ArrayCollection;
 use Doctrine\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
 
 #[ORM\Entity]//(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: 'Utilisateur')]
-class User
-{
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+    {
+    #[ORM\Column(type: 'json')]
+    private $roles = [];
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
@@ -194,5 +200,65 @@ class User
             }
         }
         return $this;
+    }
+
+    /**
+     * The public representation of the user (e.g. a username, an email address, etc.)
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+    /**
+     * @return string the hashed password for this user
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
     }
 }
